68e22f8ab628374bc76c8ea6272125c5cdfbcbfa
Stephen <Stephen>
2015-07-21 20:16:17 +0200

    updated for BioPerl/BioJava/BioPython

--- src/adapter/MainClass.java
+++ src/adapter/MainClass.java
@@ -15,29 +15,50 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		
 		String inputFile;
-		String delimiter;
+		String delimiter = null;
 		
 		System.out.println("The entered format :");
 		for(int i = 0;i<args.length ; i++)
 			System.out.print(args[i]+" ");
 		inputFile = args[0];
-		Integer.parseInt(args[1]);
-		if(args[2].equalsIgnoreCase("space"))
+		if(args[1].equalsIgnoreCase("space"))
 			delimiter = "\\s";
-		else
+		else if(args[1].equalsIgnoreCase("tab"))
 			delimiter = "\\t";
+		else
+			System.out.println("Second argument must be [space/tab]");
 		
-		Scanner param = new Scanner (args[3]);
+		Scanner param = new Scanner (args[2]);
 		param.useDelimiter(",");
-		param.nextInt();
-		param.nextInt();
-		param.nextInt();
+		int u = param.nextInt();
+		int v = param.nextInt();
+		int time = param.nextInt();
 		param.close();
-			
 			
 		try{
 			Scanner reader = new Scanner (new File(inputFile));
 			reader.useDelimiter(delimiter);
+//			int index = 1;
+//			while(reader.hasNext()){
+//				if(index>10){
+//					index=1;
+//				}
+//				
+//				if(index==u){
+//					String v1 = reader.next();
+//					++index;
+//
+//				}else if(index==v){
+//					String v2 = reader.next();
+//					++index;
+//				}
+//				else{
+//					reader.next();
+//					++index;
+//				}
+//
+//			}
+			
 			while(reader.hasNext()){
 				reader.next();
 				reader.next();
@@ -53,12 +74,11 @@ public class MainClass {
 				else {
 					reader.next();
 					String v3 = reader.next().substring(0, 4);
-					//if(v3.equals("2014")){
-					writeToLogln(v1 +"	"+ v2 + "	" + v3);
-					//}
+					writeToLogln(args[0]+"_"+v3.toString().concat(".txt"), v1 +" "+ v2);
 					reader.next();
 				}
 			}
+
 			reader.close();
 		}catch(NoSuchElementException e){
 			e.getMessage();
@@ -74,29 +94,10 @@ public class MainClass {
      *
      */
 	
-	public static void writeToLogln(String message) {
-		try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("logfile.txt", true)))) {
+	public static void writeToLogln(String filename, String message) {
+		try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)))) {
 		    //Log message with end of line
 			out.println(message);
-		    out.close();
-		}catch (IOException e) {
-			System.out.println("Error: Write operation to logfile failed.");
-		    e.printStackTrace();
-		}
-	}
-	
-	/**
-     * Responsible for logging all messages and output generated into a logfile.
-     * Without end of line.
-     *
-     * @param message
-     *            String value that needs to be logged in the file
-     *
-     */
-	public static void writeToLog(String message) {
-		try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("logfile.txt", true)))) {
-		    //Log message without end of line
-			out.print(message);
 		    out.close();
 		}catch (IOException e) {
 			System.out.println("Error: Write operation to logfile failed.");
